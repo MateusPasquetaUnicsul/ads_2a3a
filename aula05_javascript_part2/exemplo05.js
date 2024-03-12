69dfd40936bb4c927b480cd6ea362a762af5e4a0
JacaMan <Pd@outlook.com>
2024-03-12 09:44:07 -0300

    novos arquivos de exemplos

---
+++ aula05_javascript_part2/exemplo05.js
@@ -0,0 +1,18 @@
+let valorA
+let valorB
+let valorC
+let valorD
+let resultado
+
+//entrada 
+valorA = 30
+valorB = 20
+valorC = 15
+valorD = 20
+//processamento
+resultado = valorA + valorB
+resultado2 = valorC + valorD
+
+//saida
+console.log(resultado)
+console.log(resultado2)
