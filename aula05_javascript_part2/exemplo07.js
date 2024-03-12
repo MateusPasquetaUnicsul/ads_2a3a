74cc9f20ad2c54331777fa13880d03820a7fce6f
JacaMan <Pd@outlook.com>
2024-03-12 10:42:05 -0300

    Novo arquivo JS

--- aula05_javascript_part2/exemplo07.js
+++ aula05_javascript_part2/exemplo07.js
@@ -7,10 +7,36 @@ entre 35 e 40 = obsedidade II
 acima de 40 = obsedidade III
 */
 
-calcularIMC(65, 1.70)
+let retornoImc
+retornoImc = calcularIMC(65, 1.70)
+console.log(retornoImc)
+classificarIMC(retornoImc)
+
+//classificarIMC(imc)
 
 function calcularIMC(argPeso, argAltura){
-    let imc 
     imc = argPeso / (argAltura **2)
-    console.log(imc)
+    //console.log(imc)
+    return imc
+}
+
+function classificarIMC(argIMC){
+    if(argIMC < 18.5){
+        console.log('Desnutrição')
+    }
+    else if(argIMC < 25){
+        console.log('Peso normal')
+    }
+    else if(argIMC < 30){
+        console.log('Sobrepeso')
+    }
+    else if(argIMC < 35){
+        console.log('Obesidade I')
+    }
+    else if(argIMC < 40){
+        console.log('Obsedidade II')
+    }
+    else if(argIMC < 45){
+        console.log('Obsedidade III')
+    }
 }
